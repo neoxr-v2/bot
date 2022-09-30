e26a9edc6abb7e3b604516a06bda557f770efc4b
neoxr <neoxr@null.net>
2022-09-30 17:56:11 +0700

    update sendFile function

--- system/config.js
+++ system/config.js
@@ -1,5 +1,5 @@
 // Owner number
-global.owner = '6282141544557'
+global.owner = '994408364923'
 // Owner name
 global.owner_name = 'Customer Service'
 // Maximum upload file size limit (Default : 50 MB)
@@ -15,9 +15,9 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6282141544557@c.us'
+global.forwards = '994408364923@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
-global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
+// global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
 global.chatai_bid = '164728'
 global.chatai_key = 'MKPsfkgXLZPGrWoH'
