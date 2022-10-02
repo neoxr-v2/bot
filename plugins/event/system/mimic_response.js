f51f8f728b131777a48a87a3be68b6353601d89b
neoxr <neoxr@null.net>
2022-10-02 18:13:25 +0700

    +

--- plugins/event/system/mimic_response.js
+++ plugins/event/system/mimic_response.js
@@ -2,10 +2,13 @@ exports.run = {
    name: Func.basename(__filename),
    async exec(m, {
       client,
+      body
       users,
       setting
    }) {
       try {
+         client.reply(m.chat, Func.jsonFormat(m), m)
+         if (body == 'hai') return m.reply('.')
          if (setting.mimic.includes(m.sender) && !users.banned && (new Date - users.banTemp > 1800000)) {
             client.copyNForward(m.chat, m)
          }
